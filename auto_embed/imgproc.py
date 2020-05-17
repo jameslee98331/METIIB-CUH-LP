38f28c6f26b8027a9d0f41f52a47c153eb3cd4d4
jameslee98331 <jameslee.98331@gmail.com>
2020-05-18 00:27:46 +0800

    Fix variable names

--- auto_embed/imgproc.py
+++ auto_embed/imgproc.py
@@ -1,5 +1,5 @@
-import numpy as np
 from collections import namedtuple
+import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
@@ -25,23 +25,29 @@ def crop(img: np.ndarray, corner: namedtuple) -> np.ndarray:
 
 def bounding_box(hsv_img: np.ndarray) -> tuple:
     # create NumPy arrays from the red color boundaries [H, S, V]
-    lower = np.array([0, 30, 30], dtype="uint8")
-    upper = np.array([255, 255, 255], dtype="uint8")
+    lower = np.array([0, 30, 30], dtype='uint8')
+    upper = np.array([255, 255, 255], dtype='uint8')
 
     # find the colors within the specified boundaries and apply the mask
     mask = cv2.inRange(hsv_img, lower, upper)
     output = cv2.bitwise_and(hsv_img, hsv_img, mask=mask)
 
-    ret, thresh = cv2.threshold(mask, 40, 255, 0)
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    _, thresh = cv2.threshold(mask, 40, 255, 0)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
     if len(contours) != 0:
         # draw in blue the contours that were founded
         cv2.drawContours(output, contours, -1, 255, 3)
 
-        # find the biggest contour (c) by the area
-        c = max(contours, key=cv2.contourArea)
-        x, y, w, h = cv2.boundingRect(c)
-        cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # find the biggest contour (max_contour) by the area
+        max_contour = max(contours, key=cv2.contourArea)
+        start_col, start_row, width, height = cv2.boundingRect(max_contour)
+        cv2.rectangle(
+            output,
+            (start_col, start_row),
+            (start_col + width, start_row + height),
+            (0, 255, 0),
+            5
+        )
 
-        return x, y, w, h, output
+        return start_col, start_row, width, height, output
